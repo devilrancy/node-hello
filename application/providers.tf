35e007076b3ebe617e4ee5577963a730ec4967c2
Naresh Surisetty <devil@localhost.localdomain>
2019-06-16 18:35:47 +0530

    removing aws_region conflict

--- application/providers.tf
+++ application/providers.tf
@@ -1,11 +1,3 @@
-data "aws_region" "current" {}
-
-data "aws_caller_identity" "current" {}
-
-variable "aws_region" {
-  default = "ap-south-1"
-}
-
 provider "aws" {
   region = "${var.aws_region}"
 }
